4b93a6bdc0e7fecd0f9ef60418e15c405a2abfc5
Uribej2711 <juuribelo@unal.edu.co>
2024-02-26 18:34:35 -0500

    primer intento.java
    :anger:
    :anger:

---
+++ src/main/java/taller3/televisores/Control.java
@@ -0,0 +1,31 @@
+pachage taller3.televisores;
+
+public class Control {
+    private Tv tv;
+
+    public void enlazar(TV tv){
+        this.tv = tv;
+        tv.setControl(this);
+    }
+
+    public void turnOn(){
+        this.tv.turnOn();
+    }
+
+    public void turnOff(){
+        this.tv.turnOff(;)
+    }
+
+    public void setVolumen(int volumen){
+        this.tv.setVolumen(volumen);
+    }
+
+    public void getTV(){
+        return this.tv;
+    }
+
+    public void setTv(Tv tv){
+        this.tv = tv;
+    }
+
+}
---
+++ src/main/java/taller3/televisores/Marca.java
@@ -0,0 +1,17 @@
+package taller3.televisores;
+
+public class Marca {
+    private string nombre;
+
+    public  Marca(string nombre) {
+        this.setNombre(nombre);
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+}   
