21b057ec2b0f35abd217ac53f4ed182b93ec3a8f
Uribej2711 <juuribelo@unal.edu.co>
2024-02-26 21:48:27 -0500

    Update TV.java

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -9,7 +9,7 @@ public class TV {
     private Control control;
     private static int numTV = 0;
     
-    public TV(marca marca, boolean estado){
+    public TV(Marca marca, boolean estado){
         this.marca = marca;
         this.estado = estado;
         this.canal = canal = 1;
